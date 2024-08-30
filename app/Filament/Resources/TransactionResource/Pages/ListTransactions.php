11924b5c1bd2eedc3cfe8982076116e52ab05c80
Fikri <fikrisacila0@gmail.com>
2024-08-30 18:02:02 +0700

    make chart for the poultry has been sold based on month

--- app/Filament/Resources/TransactionResource/Pages/ListTransactions.php
+++ app/Filament/Resources/TransactionResource/Pages/ListTransactions.php
@@ -4,13 +4,16 @@ namespace App\Filament\Resources\TransactionResource\Pages;
 
 use App\Filament\Resources\TransactionResource;
 use App\Filament\Resources\TransactionResource\Widgets\Transaction;
+use App\Filament\Resources\TransactionResource\Widgets\TransactionChart;
 use Filament\Actions;
+use Filament\Pages\Concerns\ExposesTableToWidgets;
 use Filament\Resources\Pages\ListRecords;
 
 class ListTransactions extends ListRecords
 {
     protected static string $resource = TransactionResource::class;
 
+    use ExposesTableToWidgets;
     protected function getHeaderActions(): array
     {
         return [
@@ -21,7 +24,8 @@ class ListTransactions extends ListRecords
     protected function getHeaderWidgets(): array
     {
         return [
-            Transaction::class
+            Transaction::class,
+            TransactionChart::class
         ];
     }
 }
---
+++ app/Filament/Resources/TransactionResource/Widgets/TransactionChart.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Filament\Resources\TransactionResource\Widgets;
+
+use App\Models\Transaction;
+use Filament\Widgets\ChartWidget;
+use Filament\Widgets\Concerns\InteractsWithPageTable;
+use Flowframe\Trend\Trend;
+use Flowframe\Trend\TrendValue;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
+
+class TransactionChart extends ChartWidget
+{
+    protected static ?string $heading = 'Laporan Penjualan';
+    use InteractsWithPageTable;
+
+    protected function getData(): array
+    {
+        
+        $duck = Trend::query(Transaction::query()
+            ->whereHas('poultry', function($query) {
+                $query->where('category', 'Bebek');
+            }))
+            ->between(
+                start: now()->subMonth(6),
+                end: now()
+            )
+            ->perMonth()
+            ->sum('total');
+        $chicken = Trend::query(Transaction::query()
+            ->whereHas('poultry', function($query) {
+                $query->where('category', 'Ayam');
+            }))
+            ->between(
+                start: now()->subMonth(6),
+                end: now()
+            )
+            ->perMonth()
+            ->sum('total');
+        $duckling = Trend::query(Transaction::query()
+            ->whereHas('poultry', function($query) {
+                $query->where('category', 'Itik');
+            }))
+            ->between(
+                start: now()->subMonth(6),
+                end: now()
+            )
+            ->perMonth()
+            ->sum('total');
+        $bird = Trend::query(Transaction::query()
+            ->whereHas('poultry', function($query) {
+                $query->where('category', 'Burung');
+            }))
+            ->between(
+                start: now()->subMonth(6),
+                end: now()
+            )
+            ->perMonth()
+            ->sum('total');
+
+
+        return [
+            'datasets' => [
+                [
+                    'label' => 'Bebek',
+                    'data' => $duck->map(fn (TrendValue $value) => $value->aggregate),
+                    'borderColor' => '#36A2EB', 
+                    'backgroundColor' => 'rgba(54, 162, 235, 0.2)', 
+                ],
+                [
+                    'label' => 'Ayam',
+                    'data' => $chicken->map(fn (TrendValue $value) => $value->aggregate),
+                    'borderColor' => '#9966FF', 
+                    'backgroundColor' => 'rgba(153, 102, 255, 0.2)',
+                ],
+                [
+                    'label' => 'Itik',
+                    'data' => $duckling->map(fn (TrendValue $value) => $value->aggregate),
+                    'borderColor' => '#FF6384', 
+                    'backgroundColor' => 'rgba(255, 99, 132, 0.2)',
+                ],
+                [
+                    'label' => 'Burung',
+                    'data' => $bird->map(fn (TrendValue $value) => $value->aggregate),
+                    'borderColor' => '#FF6384', 
+                    'backgroundColor' => 'rgba(255, 99, 132, 0.2)',
+                ],
+            ],
+            'labels' => $duck->map(fn (TrendValue $value) => Carbon::parse($value->date)->format('F')),
+        ];
+    }
+
+    protected function getType(): string
+    {
+        return 'bar';
+    }
+}
